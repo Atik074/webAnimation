c65ca4dd570ce255221a8cf8241953b655301d06
Atik074 <atiksmats@gmail.com>
2024-02-03 00:47:18 +0600

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <h1 className="text-red-500 text-2xl">
+        {" "}
+        We learn web animation with basic css and Js
+      </h1>
+    </>
+  );
+}
+
+export default App;

00427a903bebcb4d72bf9608068ccdddd2c8ae3d
Atik074 <atiksmats@gmail.com>
2024-02-04 11:52:14 +0600

    loading animation

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,14 @@
 import "./App.css";
 import BoxAnimate from "./BoxAnimate/BoxAnimate";
 import Button from "./Button/Button";
+import LoadingAnimate from "./LoadingAnimate/LoadingAnimate";
 
 function App() {
   return (
     <div className="mx-6">
       <Button/>
       <BoxAnimate/>
+      <LoadingAnimate/>
     </div>
   );
 }
---
+++ src/LoadingAnimate/LoadingAnimate.tsx
@@ -0,0 +1,17 @@
+import "./LoadingAnimate.css"
+
+const LoadingAnimate = () => {
+    return (
+        <div className="main mt-20">
+            <div className="loading">
+                <div className="glass"></div>
+                <div className="glass-box">
+
+                </div>
+            </div>
+            
+        </div>
+    );
+};
+
+export default LoadingAnimate;
